ebc605c15905974e7671828817d0e0b157227199
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2019-10-02 11:09:50 +0200

    C juste lol

---
+++ server.js
@@ -0,0 +1,3 @@
+const name = require ('./wilders');
+name();
+process.exit();
--- wilders.js
+++ wilders.js
@@ -1,6 +1,7 @@
-const chalk = require('chalk');
-
-console.log(chalk.blue('Jessie C.'));
-console.log(chalk.red('Valentin'));
-console.log(chalk.green('Chris'));
-console.log(chalk.yellow('Pablo'));
+module.exports = () => {
+    const chalk = require('chalk');
+    console.log(chalk.blue('Jessie C.'));
+    console.log(chalk.red('Valentin'));
+    console.log(chalk.green('Chris'));
+    console.log(chalk.yellow('Pablo'));
+}
