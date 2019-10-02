8b7110332823b4c84c605d3ba5e01b115558f103
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2019-10-02 10:47:20 +0200

    nbofqghipg

---
+++ wilders.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+
+console.log(chalk.blue('Jessie C.'));
+console.log(chalk.red('Valentin'));
+console.log(chalk.green('Chris'));
+console.log(chalk.yellow('Pablo'));
